2537cc94b09ff6dc0556889ac54a5c7211392176
Ali Yarkin CANER <58171152+yarkincaner@users.noreply.github.com>
2024-09-17 14:27:46 +0300

    Initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+type Book = {
+  id: string
+  title: string
+}
+
+function App() {
+  const [books, setBooks] = useState<Book[]>([])
+
+  useEffect(() => {
+    const getBooks = async () => {
+      const response = await fetch('http://localhost:4000/books')
+      const data = await response.json()
+      setBooks(data)
+    }
+    getBooks()
+  }, [])
+
+  return (
+    <>
+      <ul>
+        {books.map(book => (
+          <li key={book.id}>{book.title}</li>
+        ))}
+      </ul>
+    </>
+  )
+}
+export default App
---
+++ server/routes/book.route.ts
@@ -0,0 +1,5 @@
+import express from 'express'
+import { all } from '../controllers/book.controller'
+const router = express.Router()
+router.get('/', all)
+export default router
