975c76b9e584ac3b792dd9705e3a2053eeae6076
Ali Yarkin CANER <yarkincaner@gmail.com>
2024-09-18 20:30:16 +0300

    feat(client): add flight component

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,4 +1,5 @@
 import BookYourFlight from './BookYourFlight'
+import Flights from './Flights'
 import Header from './Header'
 import SideMenu from './SideMenu'
 
@@ -7,8 +8,14 @@ export default function Home() {
     <>
       <Header />
       <div className='mt-6 grid grid-cols-1 xl:grid-cols-9 xl:space-x-8'>
-        <div className='order-last col-span-7 mt-4 xl:order-first xl:mt-0'>
+        <div className='order-last col-span-7 mt-4 space-y-6 xl:order-first xl:mt-0'>
           <BookYourFlight />
+          <div className='grid grid-cols-4 gap-2'>
+            <div className='col-span-3'>
+              <Flights />
+            </div>
+            <div className='col-span-1'>filters</div>
+          </div>
         </div>
         <div className='col-span-2'>
           <SideMenu />
