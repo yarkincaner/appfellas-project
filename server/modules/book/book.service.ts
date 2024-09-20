e55ad908f2ab814124fa6e17e60995153cc0d146
Ali Yarkin CANER <yarkincaner@gmail.com>
2024-09-20 15:06:12 +0300

    feat(server): add booked flight modules

--- server/modules/book/book.service.ts
+++
@@ -1,9 +0,0 @@
-import books from '../../booksData'
-
-export const getAllBooks = async () => {
-  // simulate a delay
-  const data = await new Promise(resolve =>
-    setTimeout(() => resolve(books), 500)
-  )
-  return data
-}
---
+++ server/modules/booked-flight/index.ts
@@ -0,0 +1,11 @@
+import * as bookedFlightController from './booked-flight.controller'
+import * as bookedFlightInterfaces from './booked-flight.interfaces'
+import BookedFlight from './booked-flight.model'
+import * as bookedFlightService from './booked-flight.service'
+
+export {
+  BookedFlight,
+  bookedFlightController,
+  bookedFlightInterfaces,
+  bookedFlightService
+}
