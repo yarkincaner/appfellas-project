ffe2922200fe94df950de1f112d2f048c7003ab4
Ali Yarkin CANER <yarkincaner@gmail.com>
2024-09-20 10:11:03 +0300

    feat(server): add error handlers

--- server/server.ts
+++ server/server.ts
@@ -2,11 +2,38 @@ import mongoose from 'mongoose'
 import app from './app'
 import config from './config/config'
 import { logger } from './modules/logger'
+import { Server } from 'http'
 
-let server: any
+let server: Server
 mongoose.connect(config.dbUrl).then(() => {
   logger.info('Connected to MongoDB')
   server = app.listen(config.port, () => {
     logger.info(`App listening at http://localhost:${config.port}`)
   })
+})
+
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      logger.info('Server closed')
+      process.exit(1)
+    })
+  } else {
+    process.exit(1)
+  }
+}
+
+const unexpectedErrorHandler = (error: string) => {
+  logger.error(error)
+  exitHandler()
+}
+
+process.on('uncaughtException', unexpectedErrorHandler)
+process.on('unhandledRejection', unexpectedErrorHandler)
+
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM received')
+  if (server) {
+    server.close()
+  }
 })
