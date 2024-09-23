595f40e2ac9c6a8d4333669a9aa4b8b4feff095e
Ali Yarkin CANER <yarkincaner@gmail.com>
2024-09-23 17:59:00 +0300

    feat(filters): airlines

--- client/src/pages/Home/Flights.tsx
+++ client/src/pages/Home/Flights.tsx
@@ -3,11 +3,29 @@ import { FC } from 'react'
 import Flight from './Flight'
 import { useGetFlights } from '@/lib/queries'
 import Icons from '@/components/Icons'
+import { useSearchParams } from 'react-router-dom'
+import { FlightFilters, SortOptions } from '@/types/flight'
 
 type Props = {}
 
 const Flights: FC<Props> = ({}) => {
-  const { data: flights, isLoading, error } = useGetFlights()
+  const [searchParams] = useSearchParams()
+  // Convert searchParams to an object that matches FlightFilters
+  const filters: FlightFilters = {
+    scheduleDate: searchParams.get('scheduleDate') || null,
+    airline: searchParams.get('airline') || null,
+    route: searchParams.get('route')
+      ? searchParams.get('route')?.split(',')
+      : null,
+    page: searchParams.get('page')
+      ? parseInt(searchParams.get('page') || '0', 10)
+      : null,
+    sort: (searchParams.get('sort') as SortOptions) || null,
+    fromDateTime: searchParams.get('fromDateTime') || null,
+    toDateTime: searchParams.get('toDateTime') || null
+  }
+
+  const { data: flights, isLoading, error } = useGetFlights(filters)
 
   if (isLoading)
     return (
@@ -20,7 +38,8 @@ const Flights: FC<Props> = ({}) => {
 
   return (
     <ScrollShadowWrapper className='no-scrollbar flex h-full flex-col gap-16 overflow-y-scroll'>
-      {flights?.map(flight => <Flight key={flight.id} flight={flight} />)}
+      {/* {flights?.map(flight => <Flight key={flight.id} flight={flight} />)} */}
+      <div>abasd</div>
     </ScrollShadowWrapper>
   )
 }
