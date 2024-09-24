5a7e4e6f18802f24de7f23a4981540610e7a093d
Ali Yarkin CANER <yarkincaner@gmail.com>
2024-09-24 10:58:51 +0300

    feat(style): add custom shadow to main card

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -11,26 +11,26 @@ const config = {
         foreground: 'hsl(var(--foreground))',
         primary: {
           DEFAULT: 'hsl(var(--primary))',
-          foreground: 'hsl(var(--primary-foreground))',
+          foreground: 'hsl(var(--primary-foreground))'
         },
         secondary: {
           DEFAULT: 'hsl(var(--secondary))',
-          foreground: 'hsl(var(--secondary-foreground))',
+          foreground: 'hsl(var(--secondary-foreground))'
         },
         card: {
           DEFAULT: 'hsl(var(--card))',
           foreground: 'hsl(var(--card-foreground))',
-          secondary: 'hsl(var(--card-secondary))',
+          secondary: 'hsl(var(--card-secondary))'
         }
       },
       borderRadius: {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
+        sm: 'calc(var(--radius) - 4px)'
       }
-    },
+    }
   },
-  plugins: [],
+  plugins: []
 } satisfies Config
 
 export default config
