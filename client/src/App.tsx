f30662faa5643d62f09d1fc925cc797f8b67db8b
Ali Yarkin CANER <yarkincaner@gmail.com>
2024-09-24 10:58:51 +0300

    feat(style): add custom shadow to main card

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <Providers>
       <main className='flex min-h-screen min-w-full justify-center'>
         <div className='container py-8 sm:py-12 md:py-16 lg:py-20 xl:py-24'>
-          <div className='min-w-full rounded-2xl bg-card shadow'>
+          <div className='neon-shadow min-w-full rounded-2xl bg-card'>
             <RouterProvider router={router} />
           </div>
         </div>
